79d362cc386cc4fab626f8034951deaf14e48571
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2023-01-22 17:44:22 -0800

    Added FrontEnd with React and Apollo Client. Added add client/project features and displayed them.

---
+++ client/src/components/AddProjectModal.jsx
@@ -0,0 +1,108 @@
+import { useState } from 'react'
+import { FaList } from 'react-icons/fa';
+import { useMutation, useQuery } from '@apollo/client';
+import { GET_PROJECTS } from '../queries/projectQueries';
+import { GET_CLIENTS } from '../queries/clientQueries';
+import { ADD_PROJECT } from '../mutations/projectMutations';
+import Spinner from './Spinner';
+
+export default function AddProjectModal() {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [status, setStatus] = useState('');
+  const [clientId, setClientId] = useState('new');
+
+
+  const [addProject] = useMutation( ADD_PROJECT , {
+    variables: { name, description, status, clientId },
+    update(cache, { data: { addProject } }) {
+      const { projects } = cache.readQuery({
+        query: GET_PROJECTS
+      });
+
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: { projects: [...projects, addProject]},
+      });
+    },
+  });
+
+
+  // Get Clients for select
+  const { loading, error, data } = useQuery(GET_CLIENTS);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    
+    if(name === '' || description === '' || status === '' || clientId === '') {
+      return alert('Please fill in all fields!')
+    }
+
+    addProject(name, description, status, clientId);
+    setName('');
+    setDescription('');
+    setStatus('new');
+    setClientId('');
+  };
+
+  if (loading) return <Spinner/>
+  if (error) return 'Something Went Wrong';
+  console.log(data.clients, "2312")
+
+  return (
+    <>
+     {!loading && !error && (
+        <>
+            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#projectModal">
+        <div className="d-flex align-items-center">
+          <FaList className='icon'/>
+          <div>New Project</div>
+        </div>
+      </button>
+
+      <div className="modal fade" id="projectModal"  aria-labelledby="projectModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="projectModalLabel">New Project</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <form onSubmit={onSubmit}>
+                <div className="mb-3">
+                  <label className="form-label">Name</label>
+                  <input type="text" className="form-control" id='name' value={name} onChange={ (e) => setName(e.target.value) }/>
+                  <label className="form-label">Description</label>
+                  <textarea className="form-control" id='description' value={description} onChange={ (e) => setDescription(e.target.value) }></textarea>
+                  <label className="form-label">Status</label>
+                  <select  id="status" className="form-select" value={status} onChange={ (e) => setStatus(e.target.value)}>
+                    <option value="new">Not Started</option>
+                    <option value="progress">In Progress</option>
+                    <option value="completed">Completed</option>
+                  </select>
+                </div>
+
+                <div className="mb-3">
+                    <label className="form-label">
+                        Client 
+                    </label>
+                    <select id="clientId" className="form-select" value={clientId} onChange={ (e) => setClientId(e.target.value)}>
+                        <option value=''>Select Client</option>
+                        {data.clients.map( (client) => (
+                            <option key={client.id} value={client.id}>{client.name}</option>
+                        ))}
+                    </select>
+                </div>
+
+                <button type="submit" data-bs-dismiss="modal" className="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+        </>
+     )}
+      
+    </>
+  )
+}
